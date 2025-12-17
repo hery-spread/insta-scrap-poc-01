3b2662e2d627f00231d546d633c6a8bf8046013a
Hery Nirintsoa <hhery889@gmail.com>
2025-12-17 18:28:34 +0300

    insta change reel response

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,8 @@ async function run() {
     headless: false,
     customConfig: {
       chromePath:
-        "./chrome/mac-142.0.7444.59/chrome-mac-x64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing",
+        // "./chrome/mac-142.0.7444.59/chrome-mac-x64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing",
+        "./chrome/linux-143.0.7499.146/chrome-linux64/chrome",
     },
     args: ["--no-sandbox"],
     turnstile: true,
@@ -22,11 +23,21 @@ async function run() {
 
   await page.setCookie(...cookies);
   await page.goto("https://www.instagram.com/reel/DO0hqB-iBcm/");
+  // await page.goto("https://www.instagram.com/p/DSXdx0biPeZ/?img_index=1");
 
   let output: Output | null = null;
   const scripts = await page.$$("script");
+  const scriptsContent: string[] = [];
   for (const scriptHandle of scripts) {
     const content = await page.evaluate((el) => el.textContent, scriptHandle);
+    // if (content && content.includes("like_count")) {
+    //   scriptsContent.push(scripts.toString());
+    //   scriptsContent.push("=========");
+    //   scriptsContent.push(
+    //     content ? JSON.stringify(JSON.parse(content ?? ""), null, 2) : "",
+    //   );
+    // }
+    // continue;
     if (
       content &&
       content.includes("xdt_api__v1__media__shortcode__web_info")
@@ -52,8 +63,35 @@ async function run() {
         console.log("[ERROR] failed to parse content", error);
       }
       break;
+    } else if (
+      content &&
+      content.includes("xdt_api__v1__clips__home__connection_v2")
+    ) {
+      try {
+        const json = JSON.parse(content);
+        const data =
+          json?.require?.[0]?.[3]?.[0]?.__bbox?.require?.[0]?.[3]?.[1]?.__bbox
+            ?.result?.data?.xdt_api__v1__clips__home__connection_v2?.edges?.[0]
+            .node?.media;
+        await writeFile("out.reel_2.json", JSON.stringify(data, null, 2));
+        if (data) {
+          output = {
+            like_count: data.like_count,
+            comment_count: data.comment_count,
+          };
+        } else {
+          console.log(
+            "[WARN] xdt_api__v1__media__shortcode__web_info exist but data not found",
+            json,
+          );
+        }
+      } catch (error) {
+        console.log("[ERROR] failed to parse content", error);
+      }
     }
   }
+
+  // await writeFile("script_post.json", scriptsContent.join("\n"));
 
   if (output) {
     console.log("[INFO] success!", output);
